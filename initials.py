cba78b8305730825b9d33a56b35a85907e2c9468
Ochine Elkanah Annang <2425403301@live.gctu.edu.gh>
2026-05-31 12:05:27 +0000

    initials assignments

---
+++ initials.py
@@ -0,0 +1,34 @@
+#File: intiala.py
+#Description: Print stylised large initials O.E.A with practice spacing.
+#Assignment Number 2
+
+#Name: Ochinie Elkanah Annang
+#Student ID: 2425403301
+#Grader: On my honour, Elkanah, this  programming assignment  is my own work
+#and I have not provided this code to any other student.
+
+
+def main():
+    # Print the small initials folloowd by the 60-character wide stylised block letters.
+    print()
+    print("...O.E.A...")
+    print()
+    print("........OOOOOO..........")
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
